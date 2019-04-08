ff8492ce2e6ce4ad9c142222e0d682dc68e62e36
Гончарик Денис <dgoncharik27@gmail.com>
2019-04-08 15:21:30 +0300

    Первый этап стилизации мобильной версии index.html

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const notify = require("gulp-notify");
+const sourcemaps = require('gulp-sourcemaps');
+// const tinypng = require('gulp-tinypng-unlimited');
+
+sass.compiler = require('node-sass');
+
+gulp.task('html', function() {
+  return gulp.src('./source/*.html')
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+})
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./source"
+      }
+  });
+});
+
+gulp.task('autoprefix', function () {
+  return gulp.src('./source/css/**/*.css')
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+      }))
+      .pipe(gulp.dest('./source/css'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./source/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+    .on("error", notify.onError({
+      message: "Error: <%= error.message %>",
+      title: "Error running sass"
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./source/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./source/sass/**/*.scss'], gulp.series('sass'));
+  gulp.watch(['./source/**/*.html'], gulp.series('html'))
+});
+
+gulp.task('default', gulp.series('sass', gulp.parallel('watch', 'browser-sync')))
