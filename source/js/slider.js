15e0730fa26c223cc68e37bd74a73e0e703ad54d
Гончарик Денис <dgoncharik27@gmail.com>
2019-05-30 13:23:55 +0300

    начал делать движение ползунка при переключении слайдов

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,20 +5,33 @@ var range = slider.querySelector('.comparisons__range');
 var toogle = range.querySelector('.comparisons__toggle');
 var btnBefore = range.querySelector('.comparisons__btn--before');
 var btnAfter = range.querySelector('.comparisons__btn--after');
+var mobileWidth = 320;
+var windowWidth = document.body.clientWidth;
 
 var startingTooglePosition = '0%';
 var endTooglePosition = '45%';
+
+console.log(toogle.offsetWidth)
 
 btnBefore.addEventListener('click', function(evt) {
   evt.preventDefault();
   console.log('before');
   imgAfter.style.width = '0%';
+  toogle.style.left = '0%';
 })
 
 btnAfter.addEventListener('click', function(evt) {
   evt.preventDefault();
   console.log('after');
   imgAfter.style.width = '100%';
+  toogle.style.left = 'calc(100% - 45px)';
 })
 
-console.log(slider, imgBefore, imgAfter, range, toogle, btnBefore, btnAfter);
+window.onresize = function(event) {
+  windowWidth = document.body.clientWidth;
+  console.log(windowWidth)
+
+  if (windowWidth > mobileWidth) {
+    console.log('bingo!!')
+  }
+};
