91dbbd403f95cc8141b6438f1e8d60e0449b663c
Гончарик Денис <dgoncharik27@gmail.com>
2019-06-01 10:57:25 +0300

    1.1

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,6 +2,7 @@ var slider = document.querySelector('.comparisons');
 var imgBefore = slider.querySelector('.comparisons__before-picture-wrapper');
 var imgAfter = slider.querySelector('.comparisons__after-picture-wrapper');
 var range = slider.querySelector('.comparisons__range');
+var progress = range.querySelector('.comparisons__progress');
 var toogle = range.querySelector('.comparisons__toggle');
 var btnBefore = range.querySelector('.comparisons__btn--before');
 var btnAfter = range.querySelector('.comparisons__btn--after');
@@ -10,22 +11,6 @@ var mobileWidth = 320;
 var tabletWidth = 768;
 var desktopWidth = 1300;
 var windowWidth = document.body.clientWidth;
-
-var getStyle = function (e, styleName) {
-  var styleValue = "";
-  if(document.defaultView && document.defaultView.getComputedStyle) {
-      styleValue = document.defaultView.getComputedStyle(e, "").getPropertyValue(styleName);
-  }
-  else if(e.currentStyle) {
-      styleName = styleName.replace(/\-(\w)/g, function (strMatch, p1) {
-          return p1.toUpperCase();
-      });
-      styleValue = e.currentStyle[styleName];
-  }
-  return styleValue;
-}
-
-console.log('ccccc', getStyle(range, 'width'))
 
 btnBefore.addEventListener('click', function(evt) {
   evt.preventDefault();
@@ -48,9 +33,28 @@ btnAfter.addEventListener('click', function(evt) {
   }
 })
 
-window.onresize = function(event) {
+window.onresize = function(evt) {
   windowWidth = document.body.clientWidth;
     imgAfter.style = null;
     imgBefore.style = null;
     toogle.style = null;
 };
+
+toogle.addEventListener('mousedown', function(evt) {
+  if (evt.which === 1) { // если клик левой кнопкой мыши
+    var toogleX = toogle.getBoundingClientRect().left + pageXOffset;
+    var progressX = progress.getBoundingClientRect().left + pageXOffset;
+    var progressWidth = progress.offsetWidth;
+    // var posLeft = toogle.offsetLeft;
+    console.log(progressX, toogleX, progressWidth);
+
+    window.onmousemove = function(evt) {
+      toogle.style.transition = 'none';
+      var newPos = evt.pageX;
+      console.log(newPos)
+      // toogle.style.left = newPos + 'px';
+
+      return false;
+    }
+  }
+})
